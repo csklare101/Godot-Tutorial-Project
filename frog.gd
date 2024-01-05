b140e8456bca549d168e9f4d8874d0572c984f37
csklare101 <77071807+csklare101@users.noreply.github.com>
2024-01-05 15:51:22 -0600

    be able to hurt the frog

--- frog.gd
+++ frog.gd
@@ -8,21 +8,25 @@ func _on_player_detection_body_entered(body):
 	if body.name == "Player":
 		chase = true
 
-
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+	
 func _physics_process(delta):
 	#Gravity for frog
 	velocity.y += gravity * delta
 	if chase:
-		get_node("AnimatedSprite2D").play("Jump")
-		player = get_node("../../Player/Player")
-		var direction = (player.position - self.position).normalized()
-		if direction.x > 0:
-			get_node("AnimatedSprite2D").flip_h = true
-		else:
-			get_node("AnimatedSprite2D").flip_h = false
-		velocity.x = direction.x * SPEED
+		if get_node("AnimatedSprite2D").animation != 'Death':
+			get_node("AnimatedSprite2D").play("Jump")
+			player = get_node("../../Player/Player")
+			var direction = (player.position - self.position).normalized()
+			if direction.x > 0:
+				get_node("AnimatedSprite2D").flip_h = true
+			else:
+				get_node("AnimatedSprite2D").flip_h = false
+			velocity.x = direction.x * SPEED
 	else:
-		get_node("AnimatedSprite2D").play("Idle")
+		if get_node("AnimatedSprite2D").animation != 'Death':
+			get_node("AnimatedSprite2D").play("Idle")
 		velocity.x = 0
 	move_and_slide()
 
@@ -30,3 +34,11 @@ func _physics_process(delta):
 func _on_player_detection_body_exited(body):
 	if body.name == "Player":
 		chase = false
+
+
+func _on_player_death_body_entered(body):
+	if body.name == 'Player':
+		chase = false
+		get_node("AnimatedSprite2D").play("Death")
+		await get_node("AnimatedSprite2D").animation_finished
+		self.queue_free()
